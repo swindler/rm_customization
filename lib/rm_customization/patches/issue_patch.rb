ebd1d03d25602f61bfdabd232960ce78c859b913
Artyom Borkowsky <artyom_borkowsky@stylesoft.by>
2016-10-07 09:09:18 +0300

    -remove field
    Signed-off-by: Artyom Borkowsky <recall@list.ru>

--- lib/rm_customization/patches/issue_patch.rb
+++ lib/rm_customization/patches/issue_patch.rb
@@ -4,7 +4,6 @@ module RmCustomization
       def self.included(base)
         base.class_eval do
           belongs_to :fixed_version, :class_name => 'VersionLimited'
-          has_many :versions_found_in, :class_name => 'VersionLimited'
           has_and_belongs_to_many :versions_included_in, :class_name => 'VersionLimited',
                                   :join_table => "versions_issues", :foreign_key => "issue_id",
                                   :association_foreign_key => "version_id"
